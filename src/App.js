35074db45a7e8ae26759edd27c7bb3d7437cb76f
Naman Vyas <dellLaptop@naman.com>
2022-05-01 20:47:16 +0530

    feature: query listTodos using API.graphql

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { API } from 'aws-amplify';
+
+const listTodoQuery = `
+query MyQuery {
+  listTodos {
+    items {
+      id
+      name
+      description
+    }
+  }
+}
+`;
 
 function App() {
+
+  async function fetchTodos() {
+
+    const apiData = await API.graphql({
+      query: listTodoQuery
+    })
+
+    console.log(apiData);
+  }
+
   return (
     <div className="App">
       <h1>Hello World !!</h1>
       <p>Version 2</p>
+      <button onClick={fetchTodos}>fetch data</button>
     </div>
   );
 }
